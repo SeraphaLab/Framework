d9dd808691454fc43bdd2461ca63b6b2a873a08e
carry0987 <carry0987@users.noreply.github.com>
2024-06-26 19:46:52 +0800

    Fix ServiceLocator

--- src/Service/ServiceLocator.php
+++ src/Service/ServiceLocator.php
@@ -1,17 +1,22 @@
 <?php
 namespace Serapha\Service;
 
+use Serapha\Core\Container;
+use Serapha\Core\Dispatcher;
+
 class ServiceLocator
 {
-    protected static $container;
+    protected static Container $container;
 
-    public static function setContainer($container)
+    public static function setContainer(Container $container)
     {
         self::$container = $container;
     }
 
-    public static function get($serviceName)
+    public static function get(string $serviceName)
     {
-        return self::$container->get($serviceName);
+        $dispatcher = new Dispatcher(self::$container);
+
+        return $dispatcher->resolve($serviceName);
     }
 }
