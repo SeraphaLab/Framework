e57cff8d862140e543e3836d41de954e55c0981c
carry0987 <carry0987@users.noreply.github.com>
2024-07-04 19:32:11 +0800

    Add strict types

--- src/Database/DataRead.php
+++ src/Database/DataRead.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Serapha\Database;
 
 use carry0987\Sanite\Models\DataReadModel;
--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Serapha\Utils;
 
 use carry0987\Utils\Utils as BaseUtils;
