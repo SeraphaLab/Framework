196524603ac43c075694b7d16b977e9ebbe478d3
carry0987 <carry0987@users.noreply.github.com>
2024-09-24 14:45:48 +0800

    Add param to Service & Model

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -8,9 +8,25 @@ use Serapha\Database\DB;
 abstract class Model
 {
     protected DB $db;
+    protected static $param = [];
 
     public function __construct(DB $db)
     {
         $this->db = $db;
+    }
+
+    public static function setParam(string|array $key, mixed $value = null): void
+    {
+        if (is_array($key)) {
+            self::$param = array_merge($key, self::$param);
+            return;
+        }
+
+        self::$param[$key] = $value;
+    }
+
+    public static function getParam(string $key = null)
+    {
+        return $key ? self::$param[$key] : self::$param;
     }
 }
