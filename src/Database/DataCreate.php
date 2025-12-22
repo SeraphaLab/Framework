2cf7a97af930a7ef983629279114901685e5eb6b
carry0987 <carry0987@users.noreply.github.com>
2025-12-22 14:53:52 +0800

    Update carry0987/sanite
    Add PostgreSQL support

--- src/Database/DataCreate.php
+++ src/Database/DataCreate.php
@@ -7,9 +7,9 @@ use carry0987\Sanite\Models\DataCreateModel;
 
 final class DataCreate extends DataCreateModel
 {
-    public function createSingle(array $queryArray, array $dataArray, bool $getAutoIncrement = false)
+    public function createSingle(array $queryArray, array $dataArray, bool $getAutoIncrement = false, ?string $sequenceName = null)
     {
-        return $this->createSingleData($queryArray, $dataArray, $getAutoIncrement);
+        return $this->createSingleData($queryArray, $dataArray, $getAutoIncrement, $sequenceName);
     }
 
     public function createMultiple(array $queryArray, array $dataArray)
